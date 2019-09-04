a5947a937a8a3f48d12023279e2d02017831904e
Dylan Mestyanek <mestyanekdylan@gmail.com>
2019-09-04 10:53:44 -0500

    mvp complete - Created user cards from followers array and rendered them to the page

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -33,7 +33,13 @@ axios
           user, and adding that card to the DOM.
 */
 
-const followersArray = [];
+const followersArray = ['LandryIrakoze', 'tetondan', 'dustinmyers', 'justsml', 'luishrd', 'bigknell'];
+
+followersArray.forEach(follower => {
+  axios
+    .get(`https://api.github.com/users/${follower}`)
+    .then(repo => cardsContainer.appendChild(createComponent(repo.data)));
+})
 
 /* Step 3: Create a function that accepts a single object as its only argument,
           Using DOM methods and properties, create a component that will return the following DOM element:
